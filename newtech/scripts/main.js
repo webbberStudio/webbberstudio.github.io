45b300fcf4fabfcb0cb1cf46b7a8da5fefc023aa
Yuriy Krugoviy <CircularBoy@users.noreply.github.com>
2018-04-01 22:24:32 +0300

    push newtech

---
+++ newtech/scripts/main.js
@@ -0,0 +1,32 @@
+// Minify header when page scroll
+const header = document.querySelector('.header');
+
+const headerScroll = function() {
+  this.scrollY < 30
+    ? header.classList.remove('header--scroll-state')
+    : header.classList.add('header--scroll-state');
+};
+headerScroll();
+
+window.onscroll = headerScroll;
+
+// Center alignment for submenus
+// Add class to menu item with submenu
+const submenus = document.querySelectorAll('.header .nav ul');
+
+submenus.forEach(function(elem) {
+  elem.parentNode.classList.add('nav-child');
+
+  elem.style.left =
+    -Math.abs((elem.offsetWidth - elem.parentNode.offsetWidth) / 2) + 'px';
+});
+
+// Hero slider initialization
+const heroSlider = $('.hero__slider ul').lightSlider({
+  item: 1,
+  controls: false,
+  onSliderLoad: function() {
+    document.querySelector('.lSPager').classList.add('container');
+    document.querySelector('.hero').classList.add('hero--slider-load');
+  }
+});
