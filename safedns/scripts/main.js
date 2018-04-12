b7697eaaea9e4957bc20b95c1ae9f3acb8abc39f
Yuriy Krugoviy <CircularBoy@users.noreply.github.com>
2018-04-12 22:39:29 +0300

    upload

---
+++ safedns/scripts/main.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+  //
+  // init vanilla slyder
+  //
+  // var slider = tns({
+  //   container: '#awards-slider',
+  //   items: 4,
+  //   mouseDrag: true,
+  //   controls: false,
+  //   nav: false
+  // });
+
+// });
